d2392d903eb78055089db947b793c22788ff90c6
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-11-25 10:37:23 +0200

    module2-task1 - Добавляет сборку проекта и розбивку на модули

---
+++ src/components/board.js
@@ -0,0 +1,11 @@
+const createBoardTemplate = () => {
+  return (
+    `
+   <section class="board container">
+    <div class="board__tasks"></div>
+   </section>
+    `
+  );
+};
+
+export {createBoardTemplate};
